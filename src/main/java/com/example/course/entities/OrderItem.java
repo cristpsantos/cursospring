12d8fa6a7ff971c901f1b311f8a92142f4e83c32
cristpsantos <pontescr@gmail.com>
2021-09-27 20:21:09 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -1,5 +1,7 @@
 package com.example.course.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -9,8 +11,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order_item")
-public class OrderItem {
-	
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
 	
@@ -59,5 +62,35 @@ public class OrderItem {
 
 	public void setPrice(Double price) {
 		this.price = price;
-	}	
+	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
